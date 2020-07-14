b7bc0679a0dd37ea4f46178338049bed9508b118
hamara91 <65727890+hamara91@users.noreply.github.com>
2020-07-14 18:51:28 -0300

    Add files via upload

---
+++ apget.js
@@ -0,0 +1,8 @@
+let $ = document.getElementById.bind(document);
+
+let title = $("img-title");
+let img = $("img-content");
+let video = $("video-content");
+let date = $("date");
+let explanation = $("explanation");
+let copyright = $("copyright");
